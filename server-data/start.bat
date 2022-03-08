7118887352196f355c4120907294e296988cb356
96drx <101216398+96drx@users.noreply.github.com>
2022-03-08 21:56:14 +0100

    FILES

--- server-data/start.bat
+++ server-data/start.bat
@@ -1,2 +1,2 @@
-cd /c C:\Users\\server-data
-C:\Users\\FXServer.exe +exec server.cfg +set onesync_enabled 1
+cd /c C:\Users\96drx\Desktop\96-ESX-BASE\server-data
+C:\Users\96drx\Desktop\96-ESX-BASE\artifacts\FXServer.exe +exec server.cfg +set onesync_enabled 1
